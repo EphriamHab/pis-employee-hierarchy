83d79fe683aa41d831e87289cbceef2214659503
EphriamHab <ephremhabtamu2015@gmail.com>
2024-06-28 09:01:45 +0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
-// components/Header.tsx
 
 import React from 'react';
 import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <div className="bg-blue-900 text-white py-4 px-6 rounded-t-lg">
+    <div className="bg-gray-800 text-white py-4 px-6 rounded-t-lg fixed top-0 left-0 w-full z-10">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Perago</h1>
         <div className="flex gap-4">
-          <Link href="/" className="text-white text-base">Home</Link>
-          <Link href="/positions" className="text-white text-base">Position</Link>
+          <Link href="/" className="text-white text-base hover:bg-blue-500 p-2">Home</Link>
+          <Link href="/positions" className="text-white text-base  hover:bg-blue-500 p-2">Position</Link>
         </div>
       </div>
     </div>
