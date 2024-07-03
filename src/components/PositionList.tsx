3662a94e9e9d6116d01bc77024d183d31ee3956e
EphriamHab <ephremhabtamu2015@gmail.com>
2024-07-03 22:02:40 +0300

    position list

--- src/components/PositionList.tsx
+++ src/components/PositionList.tsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { fetchPositions, selectPositions, selectLoading, selectError, removePosition } from '../store/positionSlice';
-import { Group, Button, Modal, Text, Box } from '@mantine/core';
-import { IconEdit, IconTrash, IconChevronDown } from '@tabler/icons-react';
-import PositionDetails from './PositionDetails';
-import { IPosition } from '../models/types';
-import { useAppDispatch } from '../store/hook';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+  fetchPositions,
+  selectPositions,
+  selectLoading,
+  selectError,
+  removePosition,
+} from "../store/positionSlice";
+import { Group, Button, Modal, Text, Box } from "@mantine/core";
+import { IconEdit, IconTrash, IconChevronDown } from "@tabler/icons-react";
+import PositionDetails from "./PositionDetails";
+import { IPosition } from "../models/types";
+import { useAppDispatch } from "../store/hook";
 
 interface PositionListProps {}
 
@@ -15,11 +21,14 @@ const PositionList: React.FC<PositionListProps> = () => {
   const loading: boolean = useSelector(selectLoading);
   const error: string | null = useSelector(selectError);
   const [expandedNodes, setExpandedNodes] = useState<number[]>([]);
-  const [selectedPositionId, setSelectedPositionId] = useState<number | null>(null);
+  const [selectedPositionId, setSelectedPositionId] = useState<number | null>(
+    null
+  );
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
-  const [positionToDelete, setPositionToDelete] = useState<IPosition | null>(null);
-
+  const [positionToDelete, setPositionToDelete] = useState<IPosition | null>(
+    null
+  );
 
   useEffect(() => {
     dispatch(fetchPositions());
@@ -27,7 +36,9 @@ const PositionList: React.FC<PositionListProps> = () => {
 
   const handleExpandToggle = (nodeId: number) => {
     setExpandedNodes((prev) =>
-      prev.includes(nodeId) ? prev.filter(id => id !== nodeId) : [...prev, nodeId]
+      prev.includes(nodeId)
+        ? prev.filter((id) => id !== nodeId)
+        : [...prev, nodeId]
     );
   };
 
@@ -49,17 +60,25 @@ const PositionList: React.FC<PositionListProps> = () => {
     setIsEditModalOpen(true);
   };
 
-  const renderTreeNodes = (parentId: number | null = null, level: number = 0) => {
+  const renderTreeNodes = (
+    parentId: number | null = null,
+    level: number = 0
+  ) => {
     return positions
-      .filter(position => position.parentId === parentId)
-      .map(position => (
+      .filter((position) => position.parentId === parentId)
+      .map((position) => (
         <Box key={position.id} mb="md">
-          <Group style={{ paddingLeft: `${level * 1.5}rem` }}  align="center">
+          <Group style={{ paddingLeft: `${level * 1.5}rem` }} align="center">
             <Group>
-              {positions.some(child => child.parentId === position.id) && (
+              {positions.some((child) => child.parentId === position.id) && (
                 <IconChevronDown
                   size={18}
-                  style={{ transform: expandedNodes.includes(position.id) ? 'rotate(180deg)' : 'rotate(0deg)', cursor: 'pointer' }}
+                  style={{
+                    transform: expandedNodes.includes(position.id)
+                      ? "rotate(180deg)"
+                      : "rotate(0deg)",
+                    cursor: "pointer",
+                  }}
                   onClick={() => handleExpandToggle(position.id)}
                 />
               )}
@@ -92,12 +111,13 @@ const PositionList: React.FC<PositionListProps> = () => {
     <>
       {loading && <Text>Loading...</Text>}
       {error && <Text color="red">{error}</Text>}
-      <Box>
-        {renderTreeNodes()}
-      </Box>
+      <Box>{renderTreeNodes()}</Box>
       {isEditModalOpen && selectedPositionId !== null && (
         <PositionDetails
-          position={positions.find(position => position.id === selectedPositionId) || null}
+          position={
+            positions.find((position) => position.id === selectedPositionId) ||
+            null
+          }
           onClose={() => {
             setIsEditModalOpen(false);
             setSelectedPositionId(null);
@@ -109,15 +129,29 @@ const PositionList: React.FC<PositionListProps> = () => {
         opened={isConfirmModalOpen}
         onClose={() => setIsConfirmModalOpen(false)}
         title="Confirm Deletion"
+        centered
       >
         <Text>
           {`Are you sure you want to delete the position "${positionToDelete?.name}"? `}
-          {positions.filter(position => position.parentId === positionToDelete?.id).length > 0 &&
-            `This will also delete ${positions.filter(position => position.parentId === positionToDelete?.id).length} child positions.`}
+          {positions.filter(
+            (position) => position.parentId === positionToDelete?.id
+          ).length > 0 &&
+            `This will also delete ${
+              positions.filter(
+                (position) => position.parentId === positionToDelete?.id
+              ).length
+            } child positions.`}
         </Text>
         <Group align="right" mt="md">
-          <Button variant="outline" onClick={() => setIsConfirmModalOpen(false)}>Cancel</Button>
-          <Button color="red" onClick={handleDelete}>Delete</Button>
+          <Button
+            variant="outline"
+            onClick={() => setIsConfirmModalOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button color="red" onClick={handleDelete}>
+            Delete
+          </Button>
         </Group>
       </Modal>
     </>
